2c0594c75674f6638b660777e3bde364354f7514
Lander Sola <landersolaaguirre@gmail.com>
2023-01-16 15:35:04 +0100

    Add changes

--- index.php
+++ index.php
@@ -27,106 +27,126 @@
             <button class="btn btn-outline-success mr-4" type="submit" name='submit'>Upload</button>
         </form>
         <form class="d-flex" action='search.php' method='GET'>
-            <input class="form-control me-2"  type="text" name='search' placeholder="Search" aria-label="Search">
+            <input class="form-control me-2" type="text" name='search' placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success" type="submit" name='search'>Search</button>
         </form>
         <form class="d-flex" action='create.php' method='POST'>
-            <input class="form-control me-2"  type="text" name='name' placeholder="Folder name:">
+            <input class="form-control me-2" type="text" name='name' placeholder="Folder name:">
             <button class="btn btn-outline-success" type="submit" name='create'>Create</button>
         </form>
-        
+
 
 
     </nav>
     <div id="mainPage">
         <div id="directoryZone">
             <div id="directoryHome">
-            <?php
-            $path = 'uploads/';
-            $files = array_values(array_diff(scandir($path), array('.', '..')));
-            echo '<ul class="list-group">';
-            foreach ($files as $file) {
-                $extension = pathinfo($file, PATHINFO_EXTENSION);
-                switch ($extension) {
-                    case "txt":
-                        $icono = "notes";
-                        break;
-                    case "doc":
-                    case "docx":
-                        $icono = "description";
-                        break;
-                    case "pdf":
-                        $icono = "picture_as_pdf";
-                        break;
-                    case "csv":
-                        $icono = "border_all";
-                        break;
-                    case "jpg":
-                    case "png":
-                        $icono = "image";
-                        break;
-                    case "ppt":
-                        $icono = "mdi mdi-file-powerpoint-box";
-                        break;
-                    case "odt":
-                        $icono = "mdi mdi-file-document-box";
-                        break;
-                    case "zip":
-                    case "rar":
-                        $icono = "mdi mdi-file-zip-box";
-                        break;
-                    case "exe":
-                        $icono = "mdi mdi-file-cog";
-                        break;
-                    case "svg":
-                        $icono = "mdi mdi-file-image";
-                        break;
-                    case "mp3":
-                        $icono = "mdi mdi-file-music-outline";
-                        break;
-                    case "mp4":
-                        $icono = "mdi mdi-file-video-outline";
-                        break;
-                    default:
-                        $icono = "folder";
-                };
-                echo '<li class="list-group-item dirItem" value="'.$path.$file.'" icon="'.$icono.'" file="'.$file.'">
+                <?php
+                $path = 'uploads/';
+
+                $files = array_values(array_diff(scandir($path), array('.', '..')));
+                echo '<ul class="list-group">';
+                foreach ($files as $file) {
+                    $extension = pathinfo($file, PATHINFO_EXTENSION);
+                    $fileSize = filesize($path . $file);
+                    $fileDate = date('Y-m-d H:i:s', filemtime($path . $file));
+                    switch ($extension) {
+                        case "txt":
+                            $icono = "notes";
+                            break;
+                        case "doc":
+                        case "docx":
+                            $icono = "description";
+                            break;
+                        case "pdf":
+                            $icono = "picture_as_pdf";
+                            break;
+                        case "csv":
+                            $icono = "border_all";
+                            break;
+                        case "jpg":
+                        case "png":
+                            $icono = "image";
+                            break;
+                        case "ppt":
+                            $icono = "co_present";
+                            break;
+                        case "odt":
+                            $icono = "description";
+                            break;
+                        case "zip":
+                        case "rar":
+                            $icono = "folder_zip";
+                            break;
+                        case "exe":
+                            $icono = "terminal";
+                            break;
+                        case "svg":
+                            $icono = "polyline";
+                            break;
+                        case "mp3":
+                            $icono = "graphic_eq";
+                            break;
+                        case "mp4":
+                            $icono = "smart_display";
+                            break;
+                        case "":
+                            $icono = "folder";
+                            break;
+                        default:
+                            $icono = "draft";
+                    };
+                    echo '<li class="list-group-item dirItem" value="' . $path . $file . '" icon="' . $icono . '" file="' . $file . '" size="' . $fileSize . '" date="' . $fileDate . '">
                       <span class="material-symbols-outlined">' . $icono . '</span> 
-                      <span><a href="'.$path.$file.'">'.$file.'</a></span>
-                      <form action="delete.php" method="post">
-                        <input type="hidden" name="file_path" value="'.$path.$file.'">
-                        <button class="btn btn-danger" value="Delete">Delete</button>
-                      </form>
+                      <span><a href="' . $path . $file . '">' . $file . '</a></span>
+                      <span class="material-symbols-outlined deleteBtn"  data-bs-toggle="modal" data-bs-target="#modal" file="'.$file.'" path="'.$path.'">delete</span>
+                      
                       <form action="rename.php" method="post">
-                        <input type="hidden" name="value" value="'.$path.$file.'">
+                        <input type="hidden" name="value" value="' . $path . $file . '">
                         <input type="text" name="rename">
                         <button class="btn btn-primary" type="submit">Rename</button>
                      </form>
 
                   </li>';
-
-
-
-            };
-            echo '</ul>';
-            ?>
+                };
+                echo '</ul>';
+                ?>
             </div>
             <div>
-               
+
             </div>
             <div>
-                
+
             </div>
         </div>
         <div id="itemInfo">
-        <span class="material-symbols-outlined" id="iconInfo"></span>
-        <h2 id="titleInfo"></h2>
-        <h3 id="sizeInfo"></h3>
-        
+            <span class="material-symbols-outlined" id="iconInfo"></span>
+            <h2 id="titleInfo"></h2>
+            <h3 id="sizeInfo"></h3>
+            <h3 id="dateInfo"></h3>
+
         </div>
     </div>
+    <div class="modal fade" id="modal" tabindex="-1"  aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modal-title"></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body" id="modal-body">
+        
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <div id="action-btn"></div>
+      </div>
+    </div>
+  </div>
+</div>
 
     <script src="script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-let filePath 
+let filePath
 let fileName
+let fileSize
 let fileIcon
+let fileDate
 let titleInfo = document.querySelector("#titleInfo")
 let iconInfo = document.querySelector("#iconInfo")
+let sizeInfo = document.querySelector("#sizeInfo")
+let dateInfo = document.querySelector("#dateInfo")
+let modalTitle = document.querySelector("#modal-title")
+let modalBody = document.querySelector("#modal-body")
+let actionBtn = document.querySelector("#action-btn")
+
+
+
 function deleteFile(file_path) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
@@ -13,11 +23,18 @@ function deleteFile(file_path) {
   xhttp.open("GET", "delete.php?file_path=" + file_path, true);
   xhttp.send();
 }
-
-function checkDirFile(path, name, icon) {
-  titleInfo.innerHTML= `${name}`
-  iconInfo.innerHTML= `${icon}`
-  console.log(icon)
+function bytesToSize(bytes) {
+  var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  if (bytes == 0) return 'n/a';
+  var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  if (i == 0) return bytes + ' ' + sizes[i];
+  return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+}
+function checkDirFile(path, name, icon, size, date) {
+  titleInfo.innerHTML = `${name}`
+  iconInfo.innerHTML = `${icon}`
+  sizeInfo.innerHTML = `${size}`
+  dateInfo.innerHTML = `${date}`
 }
 
 var items = document.querySelectorAll(".dirItem");
@@ -26,6 +43,22 @@ for (var i = 0; i < items.length; i++) {
     filePath = this.getAttribute("value")
     fileName = this.getAttribute("file")
     fileIcon = this.getAttribute("icon")
-    checkDirFile(filePath,fileName,fileIcon);
+    fileSize = this.getAttribute("size")
+    fileSize = bytesToSize(fileSize)
+    fileDate = this.getAttribute("date")
+    checkDirFile(filePath, fileName, fileIcon, fileSize, fileDate);
   });
+}
+var deleteItems = document.querySelectorAll(".deleteBtn")
+for (var i = 0; i < deleteItems.length; i++) {
+  deleteItems[i].addEventListener("click", function () {
+    fileName = this.getAttribute("file")
+    filePath = this.getAttribute("path")
+    modalTitle.innerHTML = `Delete file?`
+    modalBody.innerHTML = `<p>Are you sure that you want to DELETE the file <h5>"${fileName}"</h5>?</p>`
+    actionBtn.innerHTML = `<form action="delete.php" method="post">
+    <input type="hidden" name="file_path" value="${filePath}/${fileName}">
+    <button class="btn btn-danger" value="Delete">Delete</button>
+  </form>`
+  })
 }
