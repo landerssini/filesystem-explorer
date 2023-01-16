36cd6251cc8e75f013f804f557f284665262e9e4
Lander Sola <landersolaaguirre@gmail.com>
2023-01-16 12:47:45 +0100

    add script

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 function deleteFile(file_path) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        alert(this.responseText);
-      }
-    };
-    xhttp.open("GET", "delete.php?file_path="+file_path, true);
-    xhttp.send();
-  }
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      alert(this.responseText);
+    }
+  };
+  xhttp.open("GET", "delete.php?file_path=" + file_path, true);
+  xhttp.send();
+}
+
+function checkDirFile(e) {
+  console.log(e)
+}
+
+var items = document.querySelectorAll(".dirItem");
+for (var i = 0; i < items.length; i++) {
+  items[i].addEventListener("click", function () {
+    checkDirFile(this.getAttribute("value"));
+  });
+}
